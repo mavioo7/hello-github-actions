ca508a1cec6a1e0f2d32fc4c9838716c932829f7
mavioo7 <mavioo7@gmail.com>
2021-03-25 13:58:55 -0700

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-sh -c "echo Hello Action world my name is $INPUT_MY_NAME"
+sh -c "echo Hello Action world my name hello  is $INPUT_MY_NAME"
 sh -c "echo Just Another name is $INPUT_MY_NAME"
